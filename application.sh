cb2770074a237ba1023c1a123b1c89942b881193
kathirms <kathirvel.m@batonsystems>
2020-11-04 12:17:36 +0530

    updated comments

--- application.sh
+++ application.sh
@@ -1,9 +1,32 @@
+#validation Process
+
+validate(){
+ 
+fileNameList=$(ls config/include | sort -n)
+cd config/include
+for i in $fileNameList
+do
+
+        cat $i |  unique out
+        cat out > $i
+        rm out
+done
+
+cd ../
+
+ cat base.properties | unique out
+ cat out > base.properties
+ rm out
+  
+}
+
 
 #sort the files
 fileList=$(ls config/include | sort -n)
 
 flage=0
-
+#call the validate function
+validate
 #Find the given file is correct or Not
 for i in $fileList
 do
@@ -13,10 +36,10 @@ do
            echo "Given Property file is ::-->  $i"
 
            #get the base properties data and save in runtime.properties
-	   cat config/base.properties >config/runtime/runtime.properties
+	   cat  base.properties > runtime/runtime.properties
 
            #get given file properties and save in runTimeproperties
-	   cat config/include/$1".properties" >> config/runtime/runtime.properties
+	   cat include/$1".properties" >> runtime/runtime.properties
 
 	    
 	   flage=0
@@ -38,7 +61,6 @@ if [ $flage == 1 ]
             echo "Given File is Not Correct "
   
     fi
-
 
 
 
